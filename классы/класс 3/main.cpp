da84893fe630e357e04e1a2a42ef122c36421398
Бурдина Екатерина <117519491+chischak@users.noreply.github.com>
2023-06-16 06:23:13 +0500

    Add files via upload

---
+++ классы/класс 3/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include ".\pair.h"
+using namespace std;
+
+int main()
+{
+	Pair p;
+	Pair p2;
+	cin >> p;
+	cin >> p2;
+
+	cout << p << endl << endl;
+	cout << p2 << endl;
+	p2 = p + 1;
+	cout << p2 + 1 << endl;
+	cout << p2 - p << endl;
+	return 0;
+}
---
+++ классы/класс 3/pair.h
@@ -0,0 +1,25 @@
+#pragma once 
+#include<iostream>
+using namespace std;
+
+class Pair
+{
+private:
+	int first;
+	double second;
+public:
+	Pair();
+	Pair(int f, double s);
+	Pair(const Pair& p);
+	~Pair() {}
+	void setFirst(int first);
+	void setSecond(double second);
+	int getFirst();
+	double getSecond();
+	Pair& operator= (const Pair& p);
+	Pair& operator- (const Pair& p);
+	Pair& operator+ (int x);
+	Pair& operator+ (double x);
+	friend ostream& operator<< (ostream& out, const Pair& p);
+	friend istream& operator>> (istream& in, Pair& p);
+};
